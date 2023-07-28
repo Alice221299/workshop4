1004950c5e93f82be366c8ae389d2600509966fe
Alice <ann.bondarets22@gmail.com>
2023-07-28 12:14:00 -0500

    Some changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,20 +2,35 @@ import React, { useEffect, useState } from 'react'
 import "./home.scss"
 import imageDesktopHome from "../../assets/home/background-home-desktop.jpg"
 import imageDesktopDestination from "/images/destination/background-destination-desktop.jpg"
+import imageMobileHome from "../../assets/home/background-home-mobile.jpg"
+import imageMobileDestination from "/images/destination/background-destination-mobile.jpg"
 import Navbar from '../../components/navbar/Navbar'
 import { Outlet, useLocation } from 'react-router-dom'
+import useScreenSize from '../../hooks/useScreenSize'
 
 const Home = () => {
+  const {width} = useScreenSize()
   const location = useLocation()
   const [background, setBackground] = useState('')
+  
   useEffect(() => {
-    if (location.pathname === "/") {
-       setBackground(imageDesktopHome)
+    if (width >= 900) {
+      if (location.pathname === "/") {
+        setBackground(imageDesktopHome)
+      }
+      else if (location.pathname === "/destination"){
+        setBackground(imageDesktopDestination)
+      }
+    }else {
+      if (location.pathname === "/") {
+        setBackground(imageMobileHome)
+      }
+      else if (location.pathname === "/destination"){
+        setBackground(imageMobileDestination)
+      }
     }
-    else if (location.pathname === "/destination"){
-      setBackground(imageDesktopDestination)
-    }
-  }, [location])
+
+  }, [location, width])
 
   return (
     <>
