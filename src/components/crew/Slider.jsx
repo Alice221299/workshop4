b2aa6ec80f4149ba2dc6a440e71f175e63ba4dc7
sebastianbarrerah <sebasbh2008@hotmail.com>
2023-07-28 11:06:26 -0500

    Desplazamiento

---
+++ src/components/crew/Slider.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './slider.scss';
+import { Pagination } from 'swiper/modules';
+import crewDAta from './data';
+
+const [data, setdata] = useState([])
+
+
+
+function Slider () {
+  return (
+    <>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>dsfffafdsfsdfdsf</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default Slider
--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import HomeComponent from '../components/homeComponent/homeComponent'
+// import Crew from '../components/crew/Crew'
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
         <Routes>
             <Route path='/' element={<Home/>}>
                 <Route index element={<HomeComponent/>}/>
+                {/* <Route path='/Crew' element={<Crew/>}/> */}
             </Route>
         </Routes>
     </BrowserRouter>
