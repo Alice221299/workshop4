c4fee31063f6c24959d112c7983e9fae9407487c
Alice <ann.bondarets22@gmail.com>
2023-07-31 19:24:20 -0500

    :bug:

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './routers/Router.jsx'
-import Crew from './components/crew/Crew.jsx'
-import Slider from './components/crew/slider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <Router/>
